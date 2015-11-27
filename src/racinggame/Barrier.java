20b5aa7a26d765813ce5485f2ebcb9dcad1fff7c
louiskirstein <louis.kirstein@brentwood.ca>
2015-11-27 10:58:42 -0800

    new snake game

---
+++ src/racinggame/Barrier.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package racinggame;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+
+/**
+ *
+ * @author LK
+ */
+public class Barrier {
+    
+    public void draw(Graphics graphics){
+            graphics.setColor(getColor());
+            graphics.fill3DRect(cellData.getSystemCoordX(getX(), getY()),
+                                cellData.getSystemCoordY(getX(), getY()),
+                                cellData.getCellWidth(),
+                                cellData.getCellHeight(),
+                                true);
+    }
+    
+    public Barrier(int x, int y, Color color, CellDataProviderIntf cellData){
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.breakable = true;
+        this.cellData = cellData;
+    }
+    
+//<editor-fold defaultstate="collapsed" desc="Properties">
+    private int x, y;
+    private Color color;
+    private boolean breakable = false;
+    private CellDataProviderIntf cellData;
+    
+    
+    public int getX(){
+        return x;
+        
+    }
+    
+    /**
+     * @param x the x to set
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+    
+    /**
+     * @return the y
+     */
+    public int getY() {
+        return y;
+    }
+    
+    /**
+     * @param y the y to set
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    /**
+     * @return the color
+     */
+    public Color getColor() {
+        return color;
+    }
+    
+    /**
+     * @param color the color to set
+     */
+    public void setColor(Color color) {
+        this.color = color;
+    }
+    
+    /**
+     * @return the breakable
+     */
+    public boolean isBreakable() {
+        return breakable;
+    }
+    
+    /**
+     * @param breakable the breakable to set
+     */
+    public void setBreakable(boolean breakable) {
+        this.breakable = breakable;
+    }
+    
+//</editor-fold>
+}
--- src/racinggame/RaceTrack.java
+++ src/racinggame/RaceTrack.java
@@ -14,6 +14,7 @@ import java.awt.Image;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 
 /**
@@ -24,6 +25,7 @@ class RaceTrack extends Environment implements CellDataProviderIntf {
 
     private Grid grid;
     private Car R8;
+    private ArrayList<Barrier> barriers;
     
 
     public RaceTrack() {
@@ -31,6 +33,34 @@ class RaceTrack extends Environment implements CellDataProviderIntf {
         this.setBackground(ResourceTools.loadImageFromResource("racinggame/racing-game.jpg").getScaledInstance(1500, 900, Image.SCALE_SMOOTH));
 
         grid = new Grid(70, 36, 20, 20, new Point(20, 50), new Color(100, 100, 100, 100));
+        
+        barriers = new ArrayList<>();
+        barriers.add(new Barrier(10, 15,  Color.BLUE, this));
+        barriers.add(new Barrier(11, 15,  Color.BLUE, this));
+        barriers.add(new Barrier(12, 15,  Color.BLUE, this));
+        barriers.add(new Barrier(13, 15,  Color.BLUE, this));
+        barriers.add(new Barrier(14, 15,  Color.BLUE, this));
+        barriers.add(new Barrier(15, 15,  Color.BLUE, this));
+        barriers.add(new Barrier(16, 15,  Color.BLUE, this));
+        barriers.add(new Barrier(17, 15,  Color.BLUE, this));
+        barriers.add(new Barrier(18, 15,  Color.BLUE, this));
+        barriers.add(new Barrier(19, 15,  Color.BLUE, this));
+        barriers.add(new Barrier(20, 15,  Color.BLUE, this));
+        barriers.add(new Barrier(21, 15,  Color.BLUE, this));
+        barriers.add(new Barrier(22, 15,  Color.BLUE, this));
+        barriers.add(new Barrier(23, 15,  Color.BLUE, this));
+        barriers.add(new Barrier(24, 15,  Color.BLUE, this));
+        barriers.add(new Barrier(25, 15,  Color.BLUE, this));
+        barriers.add(new Barrier(26, 15,  Color.BLUE, this));
+        barriers.add(new Barrier(27, 15,  Color.BLUE, this));
+        barriers.add(new Barrier(27, 16,  Color.BLUE, this));
+        barriers.add(new Barrier(27, 17,  Color.BLUE, this));
+        barriers.add(new Barrier(27, 18,  Color.BLUE, this));
+        barriers.add(new Barrier(27, 19,  Color.BLUE, this));
+        barriers.add(new Barrier(27, 20,  Color.BLUE, this));
+        
+        
+         
     }
 
     @Override
@@ -73,6 +103,12 @@ class RaceTrack extends Environment implements CellDataProviderIntf {
         if (grid != null) {
             grid.paintComponent(graphics);
 
+        }
+        
+        if (barriers != null) {
+            for (int i = 0; i < barriers.size(); i++) {
+                barriers.get(i).draw(graphics);
+            }
         }
     }
 
